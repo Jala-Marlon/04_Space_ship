3c73bb2eaaa0ecfaea093725c799cf8aa969a871
Jala-Marlon <marlon.pastrana@fundacion-jala.org>
2023-06-12 19:54:05 -0500

    Tarea Clase 1

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -15,10 +15,14 @@ class Spaceship(Sprite):
 
 	def move_left(self):
 		if self.rect.left > 0:
-			self.rect.x -= 10 
+			self.rect.x -= 10
+		else:
+			self.rect.x = SCREEN_WIDTH
 	def move_right(self):
 		if self.rect.right < SCREEN_WIDTH:
-			self.rect.x += 10 
+			self.rect.x += 10
+		else:
+			self.rect.x = 0
 
 	def move_up(self):
 		if self.rect.y > SCREEN_HEIGHT // 2:
